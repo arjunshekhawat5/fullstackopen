d54eba47160c749ad6c8f5c553ffec5f67eb910c
arjun <arjunshekhawatarjun@gmail.com>
2023-09-29 22:43:45 +0530

    Add solution for exercie 1.2

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -8,9 +8,11 @@ const Header = (props) => {
 const Content = (props) => {
   console.log(props.part, props.exercises)
   return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
+    <>
+      <Part part={props.part[0]} exercises={props.exercises[0]} />
+      <Part part={props.part[1]} exercises={props.exercises[1]} />
+      <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </>
   )
 }
 
@@ -18,6 +20,15 @@ const Total = (props) => {
   console.log(props.total)
   return (
     <p>Total Number of exercises {props.total} </p>
+  )
+}
+
+const Part = (props) => {
+  console.log(props.part, props.exercises)
+  return (
+    <>
+      <p>{props.part} {props.exercises} </p>
+    </>
   )
 }
 
@@ -33,9 +44,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total total={exercises1 + exercises2 + exercises3} />
 
     </div>
