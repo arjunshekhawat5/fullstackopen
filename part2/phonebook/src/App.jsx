6aca4e37699fa6b4aecce32441fbd78cb0ddf10d
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2023-12-06 17:07:33 +0000

    Add solution for 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
-
   const [newNumber, setNewNumber] = useState('')
-
   const [filter, setFilter] = useState(' ')
+
+  useEffect(() => {
+    console.log('in effect');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('response handler', response.data);
+        setPersons(response.data)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
