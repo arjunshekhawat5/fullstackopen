e8c072de6460a6bbad593173f619cd737891b4cb
arjun <arjunshekhawatarjun@gmail.com>
2023-12-09 04:15:02 +0530

    Remove bug not filtering on first input

--- part2/phonebook/src/components/Filter.jsx
+++ part2/phonebook/src/components/Filter.jsx
@@ -1,23 +1,23 @@
 import Name from './Name'
 
-const Filter = ({persons, filter, handleFilterChange}) => {
+const Filter = ({ persons, filter, handleFilterChange }) => {
     return (
         <div>
-        <form>
-            <div>
-            filter with <input
-                value={filter}
-                onChange={handleFilterChange}
-            />
-            </div>
-        </form>
+            <form>
+                <div>
+                    filter with <input
+                        value={filter}
+                        onChange={handleFilterChange}
+                    />
+                </div>
+            </form>
             <ul>
-                {filter === ''? ' ':persons.filter(person => person.name
-                                                    .toLowerCase()
-                                                    .startsWith(filter.toLowerCase()))
-                                                    .map(person =>
-                                                    <Name key={person.name} name={person.name} number={person.number} />
-                )}
+                {filter.length === 0 ? ' ' : persons.filter(person => person.name
+                    .toLowerCase()
+                    .startsWith(filter.toLowerCase()))
+                    .map(person =>
+                        <Name key={person.name} name={person.name} number={person.number} />
+                    )}
             </ul>
         </div>
     )
