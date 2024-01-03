27597cef3e8d18252259bc89a241111963bc8167
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-03 15:15:11 +0000

    Add test function in utils for totalLikes

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.length == 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+    return likes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
