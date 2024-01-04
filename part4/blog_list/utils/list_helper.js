22ff8e947f65ddedc9b49e48a92b96329f8e48af
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-04 18:13:52 +0000

    Add test for most liked blog from given bloglist for exercise 4.5

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -4,12 +4,23 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     const likes = blogs.length == 0
-    ? 0
-    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+        ? 0
+        : blogs.reduce((sum, blog) => sum + blog.likes, 0)
     return likes
+}
+
+const mostLiked = (blogs) => {
+    if (blogs.length == 0) {
+        return null
+    }
+
+    return blogs.reduce((mostLikedBlog, currentBlog) => {
+        return currentBlog.likes > mostLikedBlog.likes ? currentBlog : mostLikedBlog
+    }, blogs[0])
 }
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostLiked
 }
