3550962eda9ab90a2eb55faeb8a5032ff2296d65
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-05 14:05:17 +0000

    Add mostLikes helper function for exercise 4.7 and debug exercise 4.6

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -31,11 +31,32 @@ const mostBlogs = (blogs) => {
     //return groupedByAuthor
     const authorBlogsArray = Object.entries(groupedByAuthor).map(([author, blogs]) => ({ author, blogs }))
 
-    const sortedAuthors = lodash.sortBy(authorerBlogsArray, (author) => -author.blogs.length)
+    const sortedAuthors = lodash.sortBy(authorBlogsArray, (author) => -author.blogs.length)
 
     return {
         author: sortedAuthors[0].author,
         blogs: sortedAuthors[0].blogs.length
+    }
+}
+
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    const groupedByAuthor = lodash.groupBy(blogs, (blog) => blog.author)
+
+    const authorBlogsArray = Object.entries(groupedByAuthor).map(([author, blogs]) => {
+        return ({
+            author,
+            likes: blogs.reduce((likes, blog) => likes + blog.likes, 0)
+        })
+    })
+
+    const sortedAuthorsByLikes = lodash.sortBy(authorBlogsArray, (author) => -author.likes)
+    return {
+        author: sortedAuthorsByLikes[0].author,
+        likes: sortedAuthorsByLikes[0].likes
     }
 }
 
@@ -44,5 +65,6 @@ module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
-    mostBlogs
+    mostBlogs,
+    mostLikes
 }
