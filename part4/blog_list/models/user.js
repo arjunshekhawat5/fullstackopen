800c88552dc0d540e1cf16d64cadad7d5824d9e1
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-16 14:01:17 +0000

    Add user schema for mongoose db

---
+++ part4/blog_list/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    name: String,
+    passwordHash: String,
+    notes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+User = mongoose.model('User', userSchema)
+module.exports = User
