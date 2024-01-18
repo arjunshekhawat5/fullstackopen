58fbf5d391fe7cc501e6055246fae3d22ba69565
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-18 13:43:52 +0000

    Add userRouter to handle user requests

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const blogRouter = require('./controller/blogs')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const userRouter = require('./controller/users')
 
 const mongoUrl = config.MONGODB_URI
 
@@ -22,7 +23,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogRouter)
-
+app.use('/api/users/', userRouter)
 
 module.exports = app
 
