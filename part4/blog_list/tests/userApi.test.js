f8c7717cb7859bed148928c41d2b1414d7dbba66
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-18 18:54:23 +0000

    Add test for get method and improve all the existing tests

--- part4/blog_list/tests/userApi.test.js
+++ part4/blog_list/tests/userApi.test.js
@@ -5,19 +5,11 @@ const User = require('../models/user')
 
 const api = supertest(app)
 
-beforeEach(async () => {
-    await User.deleteMany({})
-    const user = new User({
-        "username": "arjunshekhawat",
-        "name": "Arjun",
-        "passwordHash": "$2b$10$ZXdETM6B7yjFrP6UWuFDau6ywTr5jPiOA23T4RucVsxbjOO1/Tgzm",
-        "notes": [],
+describe('when an user is added', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
     })
 
-    await user.save()
-})
-
-describe('when an user is added', () => {
     test('with invalid username, returns 400', async () => {
         const invalidUsername = {
             username: 'A',
@@ -25,8 +17,17 @@ describe('when an user is added', () => {
             password: 'Passss'
         }
         await api
-            .post('/api/users/').send(invalidUsername)
+            .post('/api/users/')
+            .send(invalidUsername)
             .expect(400)
+            .expect(response => {
+                expect(response.body.error).toBe('Invalid Username or Password')
+            })
+
+        const response = await api.get('/api/users/')
+        expect(response.body.length).toBe(0)
+
+
     })
 
     test('with invalid password, returns 400', async () => {
@@ -38,23 +39,56 @@ describe('when an user is added', () => {
 
         await api
             .post('/api/users/')
+            .send(invalidPassword)
             .expect(400)
+            .expect(response => {
+                expect(response.body.error).toBe('Invalid Username or Password')
+            })
+
+        const response = await api.get('/api/users/')
+        expect(response.body.length).toBe(0)
+
     })
 
     test('with already existing username, returns 400', async () => {
-        const user = new User({
+        const newUser = {
             username: 'arjunshekhawat',
             name: 'Arjun',
-            password: 'testing'
-        })
+            password: '123456'
+        }
 
-        await user.save()
+        await api.post('/api/users/').send(newUser)
 
         await api
-            .post('/api/users')
-            .send(user)
+            .post('/api/users/')
+            .send(newUser)
             .expect(400)
+            .expect(response => {
+                expect(response.body.error).toBe('Username must be unique!')
+            })
+
+        const response = await api.get('/api/users/')
+        expect(response.body.length).toBe(1)
     })
+
+    test('with a valid user data, it saves the user to the db', async () => {
+        const validUser = {
+            username: 'arjunshekhawat',
+            name: 'Arjun',
+            password: '12243'
+        }
+
+        await api
+            .post('/api/users/')
+            .send(validUser)
+            .expect(201)
+
+        const response = await api.get('/api/users/')
+        expect(response.body.length).toBe(1)
+        expect(response.body[0].username).toBe('arjunshekhawat')
+
+    })
+
 })
 
 afterAll(async () => {
