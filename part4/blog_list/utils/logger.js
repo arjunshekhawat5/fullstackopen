2400ffb5b74cdf72e8f5d6c4eb533f91bdb51323
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-18 13:42:03 +0000

    add the missing quotes

--- part4/blog_list/utils/logger.js
+++ part4/blog_list/utils/logger.js
@@ -5,7 +5,7 @@ const info = (...params) => {
 }
 
 const error = (...params) => {
-    if (process.env.NODE_ENV !== test) {
+    if (process.env.NODE_ENV !== 'test') {
         console.error(...params)
     }
 }
