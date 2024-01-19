f208b4874b329d211a44d8e2b73985527c6f8682
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-19 13:43:14 +0000

    Add populate method for sending a random user associated with a blog

--- part4/blog_list/controller/blogs.js
+++ part4/blog_list/controller/blogs.js
@@ -3,29 +3,45 @@ const Blog = require('../models/blog')
 const logger = require('../utils/logger')
 
 blogRouter.get('/', async (request, response) => {
+
     logger.info('getting all blogs from db')
-    const blogs = await Blog.find({})
+
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1 })
+
     response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
-    try {
-        const savedBlog = await blog.save()
-        response.status(201).json(savedBlog)
-    }
-    catch {
+
+    const user = await User.findOne({})
+
+    const blog = new Blog({ ...request.body, user: user.id })
+
+    if (!blog.title || !blog.url) {
         response.status(400).end()
+        return
     }
+
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+
+    user.blogs = user.blogs.concat(savedBlog.id)
+    await user.save()
 })
 
 blogRouter.delete('/:id', async (request, response) => {
+
     logger.info(`deleteing blog post with id ${request.params.id}`)
+
     await Blog.findByIdAndDelete(request.params.id)
+
     response.status(204).end()
 })
 
 blogRouter.put('/:id', async (request, response) => {
+
     logger.info(`updating blog post with is ${request.params.id}`)
 
     const updatedBlog = await Blog.findByIdAndUpdate(
@@ -36,6 +52,7 @@ blogRouter.put('/:id', async (request, response) => {
     if (!updatedBlog) {
         return response.status(404).end()
     }
+
     response.json(updatedBlog)
 })
 
