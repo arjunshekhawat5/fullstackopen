ecf7a508bc7ca33bbb38e5ffe88b0ea394d1fc13
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-01-22 18:19:52 +0000

    Improve formatting of users.js

--- part4/blog_list/controller/users.js
+++ part4/blog_list/controller/users.js
@@ -4,29 +4,35 @@ const logger = require('../utils/logger')
 const bcrypt = require('bcrypt')
 
 userRouter.get('/', async (request, response) => {
-    
+
     logger.info('getting all users from the db')
 
     const users = await User
-    .find({})
-    .populate('blogs', { title: 1, author: 1, url: 1 })
+        .find({})
+        .populate('blogs', { title: 1, author: 1, url: 1 })
 
     response.json(users)
 })
 
 userRouter.post('/', async (request, response) => {
+
     logger.info('creating a new user')
+
     const { username, name, password } = request.body
 
     if (!username || !password || username.length < 3 || password.length < 3) {
+
         logger.error('invalid username or password, aborting createing new user')
+
         return response.status(400).json({ error: 'Invalid Username or Password' }).end()
     }
 
     const existingUsers = await User.findOne({ username })
 
     if (existingUsers) {
+
         logger.error('username already exists, aborting createing new user')
+
         return response.status(400).json({ error: 'Username must be unique!' }).end()
     }
 
