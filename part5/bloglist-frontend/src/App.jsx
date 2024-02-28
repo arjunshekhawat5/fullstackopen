b28e37f492dfca6cccf0f45f85bb2f577dde39f9
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-02-28 17:33:35 +0000

    Initialize exercises for part5

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  if (user === null){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+}
+
+export default App
