e158a19328bd83f2e512bd5fc5e6840c13ab2a05
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-02-29 15:07:43 +0000

    Add login form and conditional rendering for the app

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
   const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -12,14 +16,39 @@ const App = () => {
     )  
   }, [])
 
-  if (user === null){
-    return (
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try{
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+  }
+
+  const loginForm = () => (
       <div>
-        
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username: <input type='text' value={username} name='username' onChange={({taget}) => setUsername(target.value)} />
+          </div>
+          <div>
+            password: <input type='text' value={password} name='password' onChange={({taget}) => setPassword(target.value)} />
+          </div>
+        </form>
+        <button type='submit'>login</button>
       </div>
     )
-  }
-  return (
+
+  const blogForm = () => (
     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
@@ -27,6 +56,14 @@ const App = () => {
       )}
     </div>
   )
+  
+  return (
+    <div>
+      {user === null && loginForm}
+      {user !== null && blogForm}
+    </div>
+  )
+
 }
 
 export default App
