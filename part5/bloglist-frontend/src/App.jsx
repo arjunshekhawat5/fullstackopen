7496ea5bbad3869d8ddb1b0f96d859c432b2b876
arjun <arjunshekhawatarjun@gmail.com>
2024-03-04 05:07:56 +0530

    Add logout button and functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -31,7 +31,10 @@ const App = () => {
         setErrorMessage(null)
       }, 5000)
     }
+  }
 
+  const logout = () => {
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -52,7 +55,10 @@ const App = () => {
   const blogForm = () => (
     <div>
       <h2>blogs</h2>
-      <h4>{user.name} is Loggen In</h4>
+      <p>
+        {user.name} is Logged In
+        <button onClick={logout}>Logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
