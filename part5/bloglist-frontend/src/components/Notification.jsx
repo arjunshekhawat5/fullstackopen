754ca6c28b3ceb3bba7906383362ebbc08cf70a9
arjun <arjunshekhawatarjun@gmail.com>
2024-03-03 06:47:56 +0530

    Add notification component

---
+++ part5/bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,9 @@
+
+const Notification = ({ errorMessage }) => {
+    if (errorMessage) {
+        return <div className="error">{errorMessage}</div>
+    }
+    return null
+}
+
+export default Notification
