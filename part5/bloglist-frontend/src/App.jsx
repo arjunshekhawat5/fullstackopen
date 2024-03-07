3e6ac4ec65d5ff58ae21f2792da7d2589fb5b69d
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-03-07 16:00:54 +0000

    Use loginForm and createBlog components

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
+import CreateBlog from './components/CreateBlog'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -84,26 +86,13 @@ const App = () => {
 
 
   const loginForm = () => (
-    <div>
-      <h2>Log in to application</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username: <input
-            type='text'
-            value={username}
-            name='username'
-            onChange={({ target }) => setUsername(target.value)} />
-        </div>
-        <div>
-          password: <input
-            type='password'
-            value={password}
-            name='password'
-            onChange={({ target }) => setPassword(target.value)} />
-        </div>
-        <button type='submit'>login</button>
-      </form>
-    </div>
+    <LoginForm 
+      username={username}
+      password={password}
+      handleUsernameChange={({ target }) => setUsername(target.value)}
+      handlePasswordChange={({ target }) => setPassword(target.value)}
+      handleSubmit={handleLogin}
+    />
   )
 
   const blogForm = () => (
@@ -113,32 +102,17 @@ const App = () => {
         {user.name} is Logged In
         <button onClick={handleLogout}>Logout</button>
       </p>
-      <form onSubmit={handleCreateBlog}>
-        <div>title: <input
-          type='text'
-          value={title}
-          name='title'
-          onChange={({ target }) => setTitle(target.value)} />
-        </div>
-        <div>
-          author: <input
-            type='text'
-            value={author}
-            name='author'
-            onChange={({ target }) => setAuthor(target.value)} />
-        </div>
-        <div>
-          url: <input
-            type='text'
-            value={url}
-            name='url'
-            onChange={({ target }) => setUrl(target.value)} />
-        </div>
-        <button type='submit'>Create Blog</button>
-      </form>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <div>
+        <CreateBlog 
+          title={title}
+          author={author}
+          url={url}
+          handleTitleChange={({ target }) => setTitle(target.value)}
+          handleAuthorChange={({ target }) => setAuthor(target.value)}
+          handleUrlChange={({ target }) => setUrl(target.value)}
+          handleSubmit={handleCreateBlog}
+        />
+      </div>
     </div>
   )
 
