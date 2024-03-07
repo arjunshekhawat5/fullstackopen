2dd4988bfe76044061251bbbef35479222c04d4e
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-03-07 16:01:31 +0000

    Format loginForm

--- part5/bloglist-frontend/src/components/LoginForm.jsx
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -6,26 +6,26 @@ const LoginForm = ({
     password
 }) => {
     return (
-    <div>
-      <h2>Log in to application</h2>
-      <form onSubmit={handleSubmit}>
         <div>
-          username: <input
-            type='text'
-            value={username}
-            name='username'
-            onChange={handleUsernameChange} />
+            <h2>Log in to application</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                username: <input
+                    type='text'
+                    value={username}
+                    name='username'
+                    onChange={handleUsernameChange} />
+                </div>
+                <div>
+                password: <input
+                    type='password'
+                    value={password}
+                    name='password'
+                    onChange={handlePasswordChange} />
+                </div>
+                <button type='submit'>login</button>
+            </form>
         </div>
-        <div>
-          password: <input
-            type='password'
-            value={password}
-            name='password'
-            onChange={handlePasswordChange} />
-        </div>
-        <button type='submit'>login</button>
-      </form>
-    </div>
     )
 }
 
