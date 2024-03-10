1c9fd84b68f1ae603c7171ef070282885b93f88b
arjun <arjunshekhawatarjun@gmail.com>
2024-03-10 21:37:34 +0530

    Update blog component with view button and user details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    padding: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 2,
+    margin: 10
+  }
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const user = JSON.parse(window.localStorage.getItem('loggedInBlogAppUser')).name
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setVisible(prev => !prev)}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url} <br />
+        Likes {blog.likes}
+        <button>Like</button> <br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
