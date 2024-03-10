f3530ca48d22149c166728964f99a875fb7404ed
arjun <arjunshekhawatarjun@gmail.com>
2024-03-10 21:41:14 +0530

    Remove hide/view details button from create blog form

--- part5/bloglist-frontend/src/components/CreateBlog.jsx
+++ part5/bloglist-frontend/src/components/CreateBlog.jsx
@@ -4,7 +4,6 @@ const CreateBlog = ({ addBlog }) => {
     const [title, setTitle] = useState('')
     const [author, setAuthor] = useState('')
     const [url, setUrl] = useState('')
-    const [visible, setVisible] = useState(false)
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -19,9 +18,7 @@ const CreateBlog = ({ addBlog }) => {
         setUrl('')
     }
 
-    const showWhenVisible = {display: visible ? '' : 'none'}
-
-    return(
+    return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div>title: <input
@@ -29,24 +26,21 @@ const CreateBlog = ({ addBlog }) => {
                     value={title}
                     name='title'
                     onChange={(event) => setTitle(event.target.value)} />
-                    <button onClick={() => setVisible(prev => !prev)}>
-                        {visible?'hide':'view'}
-                    </button>
                 </div>
-                <div style = {showWhenVisible}>
+                <div>
                     <div>
                         author: <input
-                        type='text'
-                        value={author}
-                        name='author'
-                        onChange={(event) => setAuthor(event.target.value)} />
+                            type='text'
+                            value={author}
+                            name='author'
+                            onChange={(event) => setAuthor(event.target.value)} />
                     </div>
                     <div>
                         url: <input
-                        type='text'
-                        value={url}
-                        name='url'
-                        onChange={(event) => setUrl(event.target.value)} />
+                            type='text'
+                            value={url}
+                            name='url'
+                            onChange={(event) => setUrl(event.target.value)} />
                     </div>
                 </div>
                 <button type='submit'>Create Blog</button>
