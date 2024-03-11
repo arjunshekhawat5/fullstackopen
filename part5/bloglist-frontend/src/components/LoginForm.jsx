9dbbddee360a73966b8844d74fd6ada751451bf1
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-03-11 13:00:04 +0000

    Update login form with its own states

--- part5/bloglist-frontend/src/components/LoginForm.jsx
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -1,27 +1,31 @@
-const LoginForm = ({
-    handleSubmit,
-    handlePasswordChange,
-    handleUsernameChange,
-    username,
-    password
-}) => {
+const LoginForm = ({ handleLogin }) => {
+    const [username, setUsername] = useState(username)
+    const [password, setPassword] = useState(password)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const userCredentials = { username, password }
+        handleLogin(userCredentials)
+        setUsername('')
+        setPassword('')
+    }
     return (
         <div>
             <h2>Log in to application</h2>
             <form onSubmit={handleSubmit}>
                 <div>
-                username: <input
-                    type='text'
-                    value={username}
-                    name='username'
-                    onChange={handleUsernameChange} />
+                    username: <input
+                        type='text'
+                        value={username}
+                        name='username'
+                        onChange={(event) => setUsername(event.target.value)} />
                 </div>
                 <div>
-                password: <input
-                    type='password'
-                    value={password}
-                    name='password'
-                    onChange={handlePasswordChange} />
+                    password: <input
+                        type='password'
+                        value={password}
+                        name='password'
+                        onChange={(event) => setPassword(event.target.value)} />
                 </div>
                 <button type='submit'>login</button>
             </form>
