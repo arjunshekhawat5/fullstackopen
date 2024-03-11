4121237eef862a41cc1dfb0d0f71d71279c13b73
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-03-11 17:36:00 +0000

    Add delete and like button handlers

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react"
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, deleteBlog, likeBlog }) => {
   const [visible, setVisible] = useState(false)
+
+  const user = JSON.parse(window.localStorage.getItem('loggedInBlogAppUser')).name
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
 
   const blogStyle = {
     padding: 10,
@@ -11,21 +15,38 @@ const Blog = ({ blog }) => {
     margin: 10
   }
 
-  const showWhenVisible = { display: visible ? '' : 'none' }
-  const user = JSON.parse(window.localStorage.getItem('loggedInBlogAppUser')).name
+  const hideButton = () => {
+    return (
+      <button onClick={() => setVisible(prev => !prev)}>
+        {visible ? 'hide' : 'view'}
+      </button>
+    )
+  }
+
+  const deleteButton = () => {
+    return (
+      user === blog.user.name
+        ? <button onClick={() => deleteBlog(blog)}>Delete</button>
+        : ''
+    )
+  }
+
+  const likeButton = () => {
+    return (
+      <button onClick={() => likeBlog({ ...blog, likes: blog.likes + 1 })}>Like</button>
+    )
+  }
+
   return (
     <div style={blogStyle}>
       <div>
-        {blog.title} {blog.author}
-        <button onClick={() => setVisible(prev => !prev)}>
-          {visible ? 'hide' : 'view'}
-        </button>
+        {blog.title} {blog.author} {hideButton()}
       </div>
       <div style={showWhenVisible}>
         {blog.url} <br />
-        Likes {blog.likes}
-        <button>Like</button> <br />
-        {blog.user.name}
+        Likes {blog.likes} {likeButton()}
+        {blog.user.name} <br />
+        {deleteButton()}
       </div>
     </div>
   )
