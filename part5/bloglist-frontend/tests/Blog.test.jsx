b01f0fb5de70447eec8b36ab09dd1e0bb58ebd1a
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-03-13 17:18:55 +0000

    Add test 5.14 for view/hide button for blog details

--- part5/bloglist-frontend/tests/Blog.test.jsx
+++ part5/bloglist-frontend/tests/Blog.test.jsx
@@ -1,36 +1,59 @@
 import { render, screen } from '@testing-library/react'
 import Blog from '../src/components/Blog'
+import userEvent from '@testing-library/user-event'
 
-test('renders blog without likes and URL', () => {
-  const blog = {
-    title: 'testing blog render',
-    author: 'test user',
-    url: 'should not render.com',
-    likes: '0',
-    user: {
-      name: 'me',
-      username: 'me'
+
+describe('<Blog />', () => {
+  let container
+
+  beforeEach(() => {
+    const blog = {
+      title: 'testing blog render',
+      author: 'test user',
+      url: 'should not render.com',
+      likes: '0',
+      user: {
+        name: 'me',
+        username: 'me'
+      }
     }
-  }
 
-  const { container } = render(<Blog blog={blog} />)
+    container = render(<Blog blog={blog} />).container
 
-  let div = container.querySelector('.title-author')
+  })
 
-  expect(div).toHaveTextContent(
-    'testing blog render'
-  )
+  test('renders blog without likes and URL', () => {
+    let div = container.querySelector('.title-author')
 
-  expect(div).toHaveTextContent(
-    'test user'
-  )
+    expect(div).toHaveTextContent(
+      'testing blog render'
+    )
 
-  div = container.querySelector('.details')
+    expect(div).toHaveTextContent(
+      'test user'
+    )
 
-  expect(div).toHaveStyle(
-    'display: none'
-  )
+    div = container.querySelector('.details')
+    expect(div).toHaveStyle(
+      'display: none'
+    )
 
+  })
+
+
+  test('renders likes and url after clicking button', async () => {
+    const user = userEvent.setup()
+
+    const button = screen.getByText('view')
+
+    await user.click(button)
+
+    screen.debug()
+
+    const div = container.querySelector('.details')
+    expect(div).not.toHaveStyle(
+      'display: none'
+    )
+
+  })
 })
-
-
