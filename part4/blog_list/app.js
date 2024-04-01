a0208685302fd7468014fde6aaca23e836d19111
arjunshekhawat5 <42422394+arjunshekhawat5@users.noreply.github.com>
2024-04-01 16:36:12 +0000

    Add reset path for testing

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -28,6 +28,11 @@ app.use('/api/blogs', middleware.userExtractor, blogRouter)
 app.use('/api/users/', userRouter)
 app.use('/api/login/', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controller/testing')
+    app.use('api/testing', testingRouter)
+}
+
 module.exports = app
 
 
